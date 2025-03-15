285c679ffd3c771f775731952f6c423f490a8557
shibendan <ixc@live.com>
2025-03-15 11:09:01 +0800

    修改信息

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,12 +4,12 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
+  id: 2333,
   name: 'Subscription',
   version: 0,
-  author: 'author',
+  author: 'bayunai',
   checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  supportUri: 'https://github.com/shibendan/gkd-subscription',
   categories,
   globalGroups,
   apps: await batchImportApps(`${import.meta.dirname}/apps`),
